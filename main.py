413103650fa1f84b8de67d03c562598ceccec10b
sunil kumar <98368191+sunil8521@users.noreply.github.com>
2023-09-01 18:24:13 +0530

    Update main file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,jsonify,request,redirect
 import string
 import random
-from database import store ,add
+from database import fetch ,add
 app=Flask(__name__,template_folder='file')
 
 def short_url(length=6):
@@ -9,32 +9,32 @@ def short_url(length=6):
     result="".join(random.choice(all) for _ in range(length))
     return result
 
-@app.route('/',methods=['POST','GET'])
-def go():
-    global url_list
-    url_list=store()
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/url',methods=['POST','GET'])
+def url():
     if request.method =="POST":
         long_url=request.form['mainurl']
-        for u in url_list:
-            result = next((u['shorturl'] for key, value in u.items() if value == long_url), None)
-            if result is not None:
-                break
-        if result is None:
-            result = short_url()
-            add(result,long_url)
-        return render_template("redirect.html",val=f"{request.url_root}{result}")
-    return render_template('main.html')
+        if long_url:
+            url_list=fetch()
+            result = next((i['shorturl'] for i in url_list if long_url == i['longurl']),None)
+            if result is None:
+                result = short_url()
+                add(result,long_url)
+            return render_template("redirect.html",val=f"{request.url_root}{result}",lurl=long_url)
+        else:
+            return render_template('home.html',error_meassage="URL is required")
+    return render_template('home.html')
 
 @app.route("/<result>")
 def show(result):
-  url_list=store()
-  for u in url_list:
-    redi = next((u['longurl'] for key, value in u.items() if value == result), None)
-    if redi is not None:
-      break
-  if redi:
-    return redirect(redi)
-  else:
-    return "Not found",404
+    check_list=fetch()
+    checker=next((i['longurl'] for i in check_list if result == i['shorturl']),False)
+    if checker:
+        return redirect(str(checker))
+    else:
+        return render_template('404.html')
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
